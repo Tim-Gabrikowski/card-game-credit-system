8ee939baaadd586e1d0b074d119b020c3d9d7e0b
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-09-11 16:58:10 +0200

    changes

--- src/router/bj.js
+++ src/router/bj.js
@@ -5,12 +5,15 @@ const router = Router();
 
 const MESSAGES = {
 	master: {
-		GET_STATE: "GET_STATE",
+		GET_STATE: "MT_GTS",
 	},
 	observer: {},
-	player: {},
+	player: {
+		GET_STATE: "PL_GTS",
+	},
 	errors: {
 		GAME_NOT_FOUND: "ERR_GNF",
+		PLAYER_NOT_FOUND: "ERR_PNF",
 		CLIEND_FAIL: "ERR_CFR",
 		UNKNOWN_ACTION: "ERR_NVA",
 	},
@@ -48,14 +51,79 @@ router.post("/new-game", (req, res) => {
 	res.send(game);
 });
 
+router.post("/join/:key", (req, res) => {
+	const gKey = req.params.key;
+
+	let gi = GAMES.findIndex((g) => g.key == gKey);
+	if (gi == -1)
+		return res.status(404).send({
+			status: MESSAGES.status.FAIL,
+			error: MESSAGES.errors.GAME_NOT_FOUND,
+		});
+
+	let player = {
+		key: randomBytes(4).toString("hex"),
+		name: req.body.name || "Player #" + (GAMES[gi].players.length + 1),
+		socket: null,
+		balance: GAMES[gi].startMoney,
+		currBet: 0,
+	};
+	GAMES[gi].players.push(player);
+
+	res.send(player);
+});
+
+router.ws("/player/:gkey/:pkey", (soc, req) => {
+	const gKey = req.params.gkey;
+	const pKey = req.params.pkey;
+
+	let gi = GAMES.findIndex((g) => g.key == gKey);
+
+	if (gi == -1) {
+		return soc.send(
+			formSocResponse(
+				MESSAGES.status.FAIL,
+				{},
+				MESSAGES.errors.GAME_NOT_FOUND
+			)
+		);
+	}
+
+	let pi = GAMES[gi].players.findIndex((p) => p.key == pKey);
+	if (pi == -1) {
+		return soc.send(
+			formSocResponse(
+				MESSAGES.status.FAIL,
+				{},
+				MESSAGES.errors.PLAYER_NOT_FOUND
+			)
+		);
+	}
+	soc.send(formSocResponse(MESSAGES.status.OK, GAMES[gi].players[pi]));
+	GAMES[gi].players[pi].socket = soc;
+	soc.on("message", (msg) => {
+		onMasterSocketRequest(mKey, msg, (cb_msg) => {
+			soc.send(cb_msg);
+		});
+	});
+});
+
 router.ws("/master/:key", (soc, req) => {
 	let mKey = req.params.key;
 
 	let gi = GAMES.findIndex((g) => g.master.key == mKey);
 
-	if (gi == -1) return soc.send('{"error":"not found"}');
+	if (gi == -1) {
+		return soc.send(
+			formSocResponse(
+				MESSAGES.status.FAIL,
+				{},
+				MESSAGES.errors.GAME_NOT_FOUND
+			)
+		);
+	}
 
-	soc.send(JSON.stringify(GAMES[gi]));
+	soc.send(formSocResponse(MESSAGES.status.OK, GAMES[gi]));
 	GAMES[gi].master.socket = soc;
 	soc.on("message", (msg) => {
 		onMasterSocketRequest(mKey, msg, (cb_msg) => {
@@ -86,6 +154,41 @@ function onMasterSocketRequest(mKey, msg, callback) {
 
 	switch (req.action) {
 		case MESSAGES.master.GET_STATE:
+			callback(formSocResponse(MESSAGES.status.OK, GAMES[gi]));
+			break;
+		default:
+			callback(
+				formSocResponse(
+					MESSAGES.status.FAIL,
+					{},
+					MESSAGES.errors.UNKNOWN_ACTION
+				)
+			);
+			break;
+	}
+}
+
+function onPlayerSocketRequest(pKey, gKey, msg, callback) {
+	let req = {};
+	try {
+		req = JSON.parse(msg);
+	} catch (e) {
+		return callback("not understanding");
+	}
+	if (req.action == null || req.action == undefined) {
+		return callback(
+			JSON.stringify({
+				status: MESSAGES.status.FAIL,
+				error: MESSAGES.errors.CLIEND_FAIL,
+			})
+		);
+	}
+
+	let gi = GAMES.findIndex((g) => g.key == gKey);
+	let pi = GAMES[gi].players.findIndex((p) => p.key == pKey);
+
+	switch (req.action) {
+		case MESSAGES.player.GET_STATE:
 			callback(formSocResponse(MESSAGES.status.OK, GAMES[gi]));
 			break;
 		default:
